8ce73df8e23534182a355658814727a346a5d506
prit-ubuntu <66886724+prit-ubuntu@users.noreply.github.com>
2023-10-24 21:50:31 -0600

    fixed units

--- pages/5_RPO_Analyzer.py
+++ pages/5_RPO_Analyzer.py
@@ -54,9 +54,9 @@ def compare_sats(sat1df, sat2df):
         # # Plot RIC - 3D 
         fig_3d_ric = go.Figure()
         fig_3d_ric.add_trace(go.Scatter3d(x = st1df['r_miss'], y = st1df['i_miss'], z = st1df['c_miss'], mode="lines"))
-        scene_dict = {'xaxis': {'title': 'Radial (m)'},
-                        'yaxis': {'title': 'In-track (m)'}, 
-                        'zaxis': {'title': 'Cross-track (m)'}}
+        scene_dict = {'xaxis': {'title': 'Radial (km)'},
+                        'yaxis': {'title': 'In-track (km)'}, 
+                        'zaxis': {'title': 'Cross-track (km)'}}
         margin_dict = {'l': 0, 'r': 0, 'b': 0, 't': 0}
         camera_dict = dict(
             up=dict(x=0, y=0, z=1),
@@ -70,21 +70,21 @@ def compare_sats(sat1df, sat2df):
         
         # # Plot RIC miss magnitude
         fig_miss_mag = go.Figure()
-        fig_miss_mag.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['miss_mags'], name='Overall Miss (m)', mode="lines"))
-        fig_miss_mag.update_layout(title_text = f"Overall Miss Distance {subtitle_str}", xaxis = {'title': 'Date'}, yaxis = {'title': 'Overall Miss (m)'}, hovermode = 'x unified')
+        fig_miss_mag.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['miss_mags'], name='Overall Miss (km)', mode="lines"))
+        fig_miss_mag.update_layout(title_text = f"Overall Miss Distance {subtitle_str}", xaxis = {'title': 'Date'}, yaxis = {'title': 'Overall Miss (km)'}, hovermode = 'x unified')
         st.plotly_chart(fig_miss_mag, theme="streamlit")
         
         # # Plot RCI state differences
         fig_ric = go.Figure()
-        fig_ric.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['r_miss'], name='Radial Miss (m)', mode='lines', yaxis="y1", hovertemplate = '%{y:.2f}'))
-        fig_ric.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['i_miss'], name='In-track Miss (m)', mode='lines', yaxis="y2", hovertemplate = '%{y:.2f}'))
-        fig_ric.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['c_miss'], name='Cross-track Miss (m)', mode='lines', yaxis="y3", hovertemplate = '%{y:.2f}'))
+        fig_ric.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['r_miss'], name='Radial Miss (km)', mode='lines', yaxis="y1", hovertemplate = '%{y:.2f}'))
+        fig_ric.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['i_miss'], name='In-track Miss (km)', mode='lines', yaxis="y2", hovertemplate = '%{y:.2f}'))
+        fig_ric.add_trace(go.Scatter(x = st1df['epoch'], y = st1df['c_miss'], name='Cross-track Miss (km)', mode='lines', yaxis="y3", hovertemplate = '%{y:.2f}'))
         fig_ric.update_layout(
             # xaxis = {'title': 'Date', 'domain': [0.25, 0.9], 'tickformat' : '%Y-%m-%dT%H:%M:%SZ'},
             xaxis = {'title': 'Date', 'tickformat' : '%Y-%m-%dT%H:%M:%SZ'},
-            yaxis1  = {'title': {'text': 'Radial Miss (m)', 'standoff': 1}, 'side': 'left', 'autoshift': True, 'anchor': 'free', 'position': 0.1},
-            yaxis2 = {'title': {'text': 'In-track Miss (m)', 'standoff': 1}, 'side': 'left', 'overlaying': 'y1', 'autoshift': True, 'position': 0.2},
-            yaxis3 = {'title': {'text': 'Cross-track Miss (m)', 'standoff': 15} , 'side': 'right', 'overlaying': 'y1', 'autoshift': True, 'position': 0.9},
+            yaxis1  = {'title': {'text': 'Radial Miss (km)', 'standoff': 1}, 'side': 'left', 'autoshift': True, 'anchor': 'free', 'position': 0.1},
+            yaxis2 = {'title': {'text': 'In-track Miss (km)', 'standoff': 1}, 'side': 'left', 'overlaying': 'y1', 'autoshift': True, 'position': 0.2},
+            yaxis3 = {'title': {'text': 'Cross-track Miss (km)', 'standoff': 15} , 'side': 'right', 'overlaying': 'y1', 'autoshift': True, 'position': 0.9},
             hovermode = 'x unified',
             legend = {'orientation' : 'h', 'xanchor': 'right', 'x': 1, 'yanchor': 'bottom', 'y': -1},
             title_text=f"RIC State Difference {subtitle_str}")
@@ -95,19 +95,19 @@ def compare_sats(sat1df, sat2df):
         fig_r_i = go.Figure()
         fig_r_i.add_trace(go.Scatter(x = st1df['i_miss'], y = st1df['c_miss'], name='R', mode="lines"))
         # fig_r_i.add_vline(x=5000, line_width=1, line_dash="dash", line_color="red")
-        fig_r_i.update_layout(title_text = f"In-Track-Cross-Track Plane {subtitle_str}", xaxis = {'title': 'In-track (m)'}, yaxis = {'title': 'Cross-track (m)'}, hovermode = 'x unified')
+        fig_r_i.update_layout(title_text = f"In-Track-Cross-Track Plane {subtitle_str}", xaxis = {'title': 'In-track (km)'}, yaxis = {'title': 'Cross-track (km)'}, hovermode = 'x unified')
         st.plotly_chart(fig_r_i, theme="streamlit")
         # # 3. Plot Radial-Cross-Track Plane
         fig_r_i = go.Figure()
         fig_r_i.add_trace(go.Scatter(x = st1df['r_miss'], y = st1df['c_miss'], name='R', mode="lines"))
         # fig_r_i.add_vline(x=5000, line_width=1, line_dash="dash", line_color="red")
-        fig_r_i.update_layout(title_text = f"Radial-Cross-Track Plane {subtitle_str}", xaxis = {'title': 'Radial (m)'}, yaxis = {'title': 'Cross-track (m)'}, hovermode = 'x unified')
+        fig_r_i.update_layout(title_text = f"Radial-Cross-Track Plane {subtitle_str}", xaxis = {'title': 'Radial (km)'}, yaxis = {'title': 'Cross-track (km)'}, hovermode = 'x unified')
         st.plotly_chart(fig_r_i, theme="streamlit")
         # # 4. Plot In-Track-Radial Plane
         fig_r_i = go.Figure()
         fig_r_i.add_trace(go.Scatter(x = st1df['i_miss'], y = st1df['r_miss'], name='R', mode="lines"))
         # fig_r_i.add_vline(x=5000, line_width=1, line_dash="dash", line_color="red")
-        fig_r_i.update_layout(title_text = f"In-Track-Radial Plane {subtitle_str}", xaxis = {'title': 'In-track (m)'}, yaxis = {'title': 'Radial (m)'}, hovermode = 'x unified')
+        fig_r_i.update_layout(title_text = f"In-Track-Radial Plane {subtitle_str}", xaxis = {'title': 'In-track (km)'}, yaxis = {'title': 'Radial (km)'}, hovermode = 'x unified')
         st.plotly_chart(fig_r_i, theme="streamlit")
 
 
